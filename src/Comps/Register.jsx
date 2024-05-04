8508185c58d25ba82b946722be2bfbfbad2a22fa
noam1134 <noamchuri96@gmail.com>
2024-05-05 02:22:57 +0300

    tried

--- src/Comps/Register.jsx
+++ src/Comps/Register.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Button,
   TextField,
@@ -29,117 +29,75 @@ const Register = () => {
   });
   const navigate = useNavigate();
 
-  const validateEmail = (email) => {
-    const re =
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-  };
+  useEffect(() => {
+    // Load Cloudinary script and initialize widget on mount
+    if (!window.cloudinary) {
+      const script = document.createElement("script");
+      script.src = "https://widget.cloudinary.com/v2.0/global/all.js";
+      script.onload = () => initializeWidget();
+      document.body.appendChild(script);
+    } else {
+      initializeWidget();
+    }
+  }, []);
 
-  const validatePassword = (password) => {
-    const re = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/; // Minimum eight characters, at least one letter and one number
-    return re.test(password);
+  const initializeWidget = () => {
+    const myWidget = window.cloudinary.createUploadWidget(
+      {
+        cloudName: "dzl5twp4b",
+        uploadPreset: "esseufv8",
+        sources: ["local", "url", "camera", "image_search"],
+        googleApiKey: "<YOUR_GOOGLE_API_KEY>", // Include if using image_search
+        showAdvancedOptions: true,
+        cropping: true,
+        multiple: false,
+        defaultSource: "local",
+        styles: {
+          palette: {
+            window: "#FFFFFF",
+            windowBorder: "#90A0B3",
+            tabIcon: "#0078FF",
+            menuIcons: "#5A616A",
+            textDark: "#000000",
+            textLight: "#FFFFFF",
+            link: "#0078FF",
+            action: "#FF620C",
+            inactiveTabIcon: "#0E2F5A",
+            error: "#F44235",
+            inProgress: "#0078FF",
+            complete: "#20B832",
+            sourceBg: "#E4EBF1",
+          },
+        },
+      },
+      (error, result) => {
+        if (!error && result && result.event === "success") {
+          console.log("Done! Here is the image info: ", result.info);
+          setForm((prev) => ({ ...prev, image: result.info.secure_url }));
+        }
+      }
+    );
+
+    document.getElementById("upload_widget").addEventListener(
+      "click",
+      function () {
+        myWidget.open();
+      },
+      false
+    );
   };
 
   const handleChange = (event) => {
     const { name, value } = event.target;
     setForm((prev) => ({ ...prev, [name]: value }));
-
-    if (name === "email") {
-      setErrors((prev) => ({
-        ...prev,
-        email: validateEmail(value) ? "" : "Invalid email address",
-      }));
-    } else if (name === "password") {
-      setErrors((prev) => ({
-        ...prev,
-        password: validatePassword(value)
-          ? ""
-          : "Password should be at least 8 characters long and include at least one letter and one number",
-      }));
-    } else if (name === "firstName") {
-      setErrors((prev) => ({
-        ...prev,
-        firstName: value ? "" : "First name is required",
-      }));
-    } else if (name === "lastName") {
-      setErrors((prev) => ({
-        ...prev,
-        lastName: value ? "" : "Last name is required",
-      }));
-    } else if (name === "hospitalId") {
-      setErrors((prev) => ({
-        ...prev,
-        hospitalId: value ? "" : "Hospital ID is required",
-      }));
-    } else if (name === "image") {
-      setErrors((prev) => ({
-        ...prev,
-        image: value ? "" : "Image is required",
-      }));
-    }
-  };
-
-  const handleImageChange = (event) => {
-    setForm((prev) => ({ ...prev, image: event.target.files[0] }));
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    if (
-      !errors.email &&
-      !errors.password &&
-      !errors.firstName &&
-      !errors.lastName &&
-      !errors.hospitalId &&
-      !errors.image &&
-      form.email &&
-      form.password &&
-      form.firstName &&
-      form.lastName &&
-      form.hospitalId &&
-      form.image
-    ) {
-      try {
-        const response = await fetch(
-          "https://localhost:7115/api/HospitalManager/Registration",
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(form),
-          }
-        );
-        const data = await response.json();
-        if (response.ok) {
-          console.log("Registration successful");
-          const { password, ...managerDetails } = form; // Exclude password from the form data
-          console.log("Look==============>" + JSON.stringify(managerDetails));
-          sessionStorage.setItem("user", JSON.stringify(managerDetails));
-          navigate("/main"); // Navigate to the main page upon successful registration
-        } else {
-          console.error("Registration failed:", data.message);
-          // Optionally display this error on the UI
-        }
-      } catch (error) {
-        console.error("Error during registration:", error);
-        // Optionally display this error on the UI
-      }
-    } else {
-      // If any field is invalid or empty, set an error for the hospital select
-      setErrors((prev) => ({
-        ...prev,
-        hospitalId: form.hospitalId ? "" : "Hospital is required",
-      }));
-    }
+    // Implement validation checks here before submitting the form
+    // Here you would typically also include a fetch or axios post request to your backend API
+    navigate("/main"); // Navigate to main dashboard after registration
   };
-
-  // Array of hospital IDs for dropdown
-  const hospitalOptions = [
-    { id: "1", name: "Hospital 1" },
-    { id: "2", name: "Hospital 2" },
-    { id: "3", name: "Hospital 3" },
-  ];
 
   return (
     <Box className="registerForm">
@@ -154,10 +112,8 @@ const Register = () => {
           name="email"
           value={form.email}
           onChange={handleChange}
-          error={!!errors.email}
-          helperText={errors.email}
           fullWidth
-          margin="normal"
+          margin="dense"
         />
         <TextField
           label="First Name"
@@ -166,10 +122,8 @@ const Register = () => {
           name="firstName"
           value={form.firstName}
           onChange={handleChange}
-          error={!!errors.firstName}
-          helperText={errors.firstName}
           fullWidth
-          margin="normal"
+          margin="dense"
         />
         <TextField
           label="Last Name"
@@ -178,10 +132,8 @@ const Register = () => {
           name="lastName"
           value={form.lastName}
           onChange={handleChange}
-          error={!!errors.lastName}
-          helperText={errors.lastName}
           fullWidth
-          margin="normal"
+          margin="dense"
         />
         <TextField
           label="Password"
@@ -190,46 +142,42 @@ const Register = () => {
           name="password"
           value={form.password}
           onChange={handleChange}
-          error={!!errors.password}
-          helperText={errors.password}
           fullWidth
-          margin="normal"
+          margin="dense"
         />
         <Select
-          label="Hospital"
+          label="Select Hospital"
           variant="outlined"
           name="hospitalId"
           value={form.hospitalId}
           onChange={handleChange}
-          error={!!errors.hospitalId}
           fullWidth
           displayEmpty
-          margin="normal"
+          margin="dense"
         >
-          <MenuItem value="" disabled>
-            Select Hospital
-          </MenuItem>
-          {hospitalOptions.map((hospital) => (
-            <MenuItem key={hospital.id} value={hospital.id}>
-              {hospital.name}
-            </MenuItem>
-          ))}
+          {/* Populate these options based on your actual data */}
+          <MenuItem value="">Select Hospital</MenuItem>
+          <MenuItem value="1">Hospital 1</MenuItem>
+          <MenuItem value="2">Hospital 2</MenuItem>
+          <MenuItem value="3">Hospital 3</MenuItem>
         </Select>
-        {errors.hospitalId && (
-          <Typography color="error" style={{ marginBottom: "20px" }}>
-            {errors.hospitalId}
-          </Typography>
-        )}
-        <input
-          accept="image/*"
-          type="file"
-          onChange={handleImageChange}
-          style={{ display: "block", marginBottom: "20px" }}
-        />
-        {errors.image && (
-          <Typography color="error" style={{ marginBottom: "20px" }}>
-            {errors.image}
-          </Typography>
+        <Button
+          id="upload_widget"
+          type="button"
+          variant="contained"
+          color="primary"
+          fullWidth
+        >
+          Upload Image
+        </Button>
+        {form.image && (
+          <Box mt={2} mb={2}>
+            <img
+              src={form.image}
+              alt="Uploaded Profile"
+              style={{ width: "100%" }}
+            />
+          </Box>
         )}
         <Button type="submit" variant="contained" color="primary" fullWidth>
           Register
