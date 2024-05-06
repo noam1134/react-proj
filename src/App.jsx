b4e32f680e9c52c4465e6fb4b129d36890c2bc01
noam1134 <noamchuri96@gmail.com>
2024-05-06 14:16:21 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Register from "./Comps/Register";
 import Login from "./Comps/Login";
 import Main from "./Comps/Main";
@@ -31,6 +31,7 @@ function App() {
       <BrowserRouter>
         <div className="App">
           <Routes>
+            <Route path="/" element={<Navigate to="/login" />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/main" element={<Main />} />
